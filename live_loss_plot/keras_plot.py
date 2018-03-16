d4ea9f0d444579401913659360502b74cb0d0a1d
Piotr Migdał <pmigdal@gmail.com>
2018-03-16 18:49:14 +0100

    adjustable columns + code example

--- live_loss_plot/keras_plot.py
+++ live_loss_plot/keras_plot.py
@@ -1,10 +1,12 @@
+from __future__ import division
+
 import matplotlib.pyplot as plt
 from keras.callbacks import Callback
 from IPython.display import clear_output
-#from matplotlib.ticker import FormatStrFormatter
 
 # TODO
-# object-oriented API
+# * object-oriented API
+# * only integer ticks
 
 metric2printable = {
     "acc": "Accuracy",
@@ -19,13 +21,20 @@ metric2printable = {
 }
 
 class PlotLosses(Callback):
-    def __init__(self, figsize=None, dynamic_x_axis=False):
+    def __init__(self, figsize=None, cell_size=(6, 4), dynamic_x_axis=False, max_cols=2):
         self.figsize = figsize
+        self.cell_size = cell_size
         self.dynamic_x_axis = dynamic_x_axis
+        self.max_cols = max_cols
 
     def on_train_begin(self, logs={}):
 
         self.base_metrics = [metric for metric in self.params['metrics'] if not metric.startswith('val_')]
+        if self.figsize is None:
+            self.figsize = (
+                self.max_cols * self.cell_size[0],
+                ((len(self.base_metrics) + 1) // self.max_cols + 1) * self.cell_size[1]
+            )
         self.loss_metric = self.model.loss
         self.max_epoch = self.params['epochs']
 
@@ -38,18 +47,20 @@ class PlotLosses(Callback):
         plt.figure(figsize=self.figsize)
 
         for metric_id, metric in enumerate(self.base_metrics):
-            plt.subplot(1, len(self.base_metrics), metric_id + 1)
+            plt.subplot((len(self.base_metrics) + 1) // self.max_cols + 1, self.max_cols, metric_id + 1)
 
             if not self.dynamic_x_axis:
-                plt.xlim(xmax=self.max_epoch)
+                plt.xlim(1, self.max_epoch)
 
             plt.plot(range(1, len(self.logs) + 1),
                      [log[metric] for log in self.logs],
                      label="training")
+
             if self.params['do_validation']:
                 plt.plot(range(1, len(self.logs) + 1),
                          [log['val_' + metric] for log in self.logs],
                          label="validation")
+
             if metric == 'loss':
                 plt.title(metric2printable.get(self.loss_metric, self.loss_metric) + " (cost function)")
             else:
