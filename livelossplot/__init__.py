2731808deb1fd375651706cb67f9313f984c28f8
Piotr Migdał <pmigdal@gmail.com>
2018-03-19 14:51:02 +0100

    conditional import of PlotLossesKeras

--- livelossplot/__init__.py
+++ livelossplot/__init__.py
@@ -1,6 +1,7 @@
 from .core import draw_plot
 from .generic_plot import PlotLosses
-from .keras_plot import PlotLossesKeras
-
-# TODO:
-# * import keras plot only if there is keras
+try:
+    from .keras_plot import PlotLossesKeras
+except:
+    # import keras plot only if there is keras
+    pass
