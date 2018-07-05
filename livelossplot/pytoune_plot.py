0d99221e647552650489fc0f71afea47d6bf7fa9
Frédérik Paradis <fredy_14@live.fr>
2018-07-05 13:50:59 -0400

    Add a pytoune_plot package with a Python notebook example.

---
+++ livelossplot/pytoune_plot.py
@@ -0,0 +1,18 @@
+from pytoune.framework import Callback
+from .generic_plot import PlotLosses
+
+class PlotLossesPytoune(Callback):
+    def __init__(self, **kwargs):
+        super(PlotLossesPytoune, self).__init__()
+        self.liveplot = PlotLosses(**kwargs)
+
+    def on_train_begin(self, logs):
+        metrics = ['loss'] + self.model.metrics_names
+        self.metrics = list(metrics)
+        self.metrics += ['val_' + metric for metric in metrics]
+
+    def on_epoch_end(self, epoch, logs):
+        metric_logs = {metric:logs[metric] for metric in self.metrics
+                                    if metric in logs}
+        self.liveplot.update(metric_logs)
+        self.liveplot.draw()
