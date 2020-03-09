f0f1d932e110d707d4bb1266554c4c4d711dded6
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-03-09 11:42:10 +0100

    add main logger properties as defaults with type hints

--- livelossplot/test_main_logger.py
+++ livelossplot/test_main_logger.py
@@ -1,3 +1,5 @@
+import re
+
 from livelossplot.main_logger import MainLogger
 
 
@@ -10,8 +12,36 @@ def test_main_logger_one():
 
 
 def test_main_logger_more():
-    logger = MainLogger()
-    logger.groups = {'acccuracy': ['acc', 'val_acc'], 'log-loss': ['loss', 'val_loss']}
+    groups = {'acccuracy': ['acc', 'val_acc'], 'log-loss': ['loss', 'val_loss']}
+    logger = MainLogger(groups=groups)
+    logger.update({
+        'acc': 0.5,
+        'val_acc': 0.4,
+        'loss': 1.2,
+        'val_loss': 1.1
+    })
+    logger.update({
+        'acc': 0.55,
+        'val_acc': 0.45,
+        'loss': 1.1,
+        'val_loss': 1.0
+    })
+    logger.update({
+        'acc': 0.65,
+        'val_acc': 0.55,
+        'loss': 1.0,
+        'val_loss': 0.9
+    })
+    grouped_log_history = logger.grouped_log_history()
+    assert len(grouped_log_history) == 2
+    assert len(grouped_log_history['acccuracy']) == 2
+    assert len(grouped_log_history['acccuracy']['val_acc']) == 3
+    assert len(grouped_log_history['log-loss']['loss']) == 3
+
+
+def test_main_logger_three():
+    group_patterns = {'acccuracy': re.compile(r'.*acc$'), 'log-loss': re.compile(r'.*loss$')}
+    logger = MainLogger(group_patterns=group_patterns)
     logger.update({
         'acc': 0.5,
         'val_acc': 0.4,
