4cd26b0019d7aa47e236a0d894e83528e31f07a0
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-03-10 11:22:38 +0100

    extrema print plugin

--- livelossplot/output_plugins/base_output.py
+++ livelossplot/output_plugins/base_output.py
@@ -1,3 +1,6 @@
 class BaseOutput:
     def send(self, *args, **kwargs):
         raise NotImplementedError()
+
+    def close(self):
+        pass
---
+++ livelossplot/output_plugins/test_extrema_print.py
@@ -0,0 +1,38 @@
+from livelossplot.plot_losses import PlotLosses
+from . import extrema_print
+
+
+def test_extrema_print():
+    groups = {'acccuracy': ['acc', 'val_acc'], 'log-loss': ['loss', 'val_loss']}
+    plugin = extrema_print.ExtremaPrint()
+    outputs = (plugin,)
+    liveplot = PlotLosses(outputs=outputs, groups=groups)
+    liveplot.update({
+        'acc': 0.5,
+        'val_acc': 0.4,
+        'loss': 1.2,
+        'val_loss': 1.1
+    })
+    liveplot.update({
+        'acc': 0.55,
+        'val_acc': 0.45,
+        'loss': 1.1,
+        'val_loss': 1.0
+    })
+    liveplot.update({
+        'acc': 0.65,
+        'val_acc': 0.35,
+        'loss': 0.5,
+        'val_loss': 0.9
+    })
+    liveplot.update({
+        'acc': 0.65,
+        'val_acc': 0.55,
+        'loss': 1.0,
+        'val_loss': 0.9
+    })
+    liveplot.send()
+    assert len(plugin.extrema_cache['loss']) == 3
+    assert plugin.extrema_cache['val_acc']['min']  == 0.35
+    assert plugin.extrema_cache['val_acc']['max']  == 0.55
+    assert plugin.extrema_cache['val_acc']['current']  == 0.55
