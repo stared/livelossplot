bc86d37d688f9025ce397c86eb7a674eceaaead5
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-03-11 10:10:54 +0100

    move extrema print to plot losses default

--- livelossplot/plot_losses.py
+++ livelossplot/plot_losses.py
@@ -3,10 +3,11 @@ from typing import Type, List
 from livelossplot.main_logger import MainLogger
 from livelossplot.output_plugins.base_output import BaseOutput
 from livelossplot.output_plugins.matplotlib import Matplotlib
+from livelossplot.output_plugins.extrema_print import ExtremaPrint
 
 
 class PlotLosses:
-    def __init__(self, outputs: List[Type[BaseOutput]] = (Matplotlib(),), **kwargs):
+    def __init__(self, outputs: List[Type[BaseOutput]] = (Matplotlib(), ExtremaPrint()), **kwargs):
         self.logger = MainLogger(**kwargs)
         self.outputs = outputs
 
