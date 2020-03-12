e838939d2265844c368de64c942e5e2b91151f7d
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-03-12 12:02:07 +0100

    docstrings for output plugins

--- livelossplot/output_plugins/neptune.py
+++ livelossplot/output_plugins/neptune.py
@@ -8,16 +8,18 @@ class NeptuneLogger(BaseOutput):
     """See: https://github.com/neptune-ai/neptune-client
     YOUR_API_TOKEN and USERNAME/PROJECT_NAME
     """
-
     def __init__(self, api_token: str, project_qualified_name: str):
+        """Set secrets and create experiment"""
         self.neptune = neptune
         self.neptune.init(api_token=api_token, project_qualified_name=project_qualified_name)
         self.neptune.create_experiment()
 
     def close(self):
+        """Close connection"""
         self.neptune.stop()
 
     def send(self, logger: MainLogger):
+        """Send collected metrics to neptune server"""
         for name, log_items in logger.log_history.items():
             last_log_item = log_items[-1]
             self.neptune.send_metric(name, last_log_item.value)
