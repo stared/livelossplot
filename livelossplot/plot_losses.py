d41c61b5219704a2df8c8581714d9424893ad977
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-03-12 12:02:34 +0100

    docstrings for liveelossplot core

--- livelossplot/plot_losses.py
+++ livelossplot/plot_losses.py
@@ -1,4 +1,5 @@
-from typing import Type, List
+import warnings
+from typing import Type, Tuple
 
 from livelossplot.main_logger import MainLogger
 from livelossplot.output_plugins.base_output import BaseOutput
@@ -7,13 +8,27 @@ from livelossplot.output_plugins.extrema_print import ExtremaPrint
 
 
 class PlotLosses:
-    def __init__(self, outputs: List[Type[BaseOutput]] = (Matplotlib(), ExtremaPrint()), **kwargs):
+    """
+    Class collect metrics from the training engine and send it to plugins, when send is called
+    """
+    def __init__(self, outputs: Tuple[Type[BaseOutput]] = (Matplotlib(), ExtremaPrint()), **kwargs):
+        """
+        :param outputs: list of callbacks (outputs) which are called with send method
+        :param kwargs: key-arguments which are passed to main logger
+        """
         self.logger = MainLogger(**kwargs)
         self.outputs = outputs
 
     def update(self, *args, **kwargs):
+        """update logs with arguments that will be passed to main logger"""
         self.logger.update(*args, **kwargs)
 
     def send(self):
+        """Method will send logs to every output class"""
         for output in self.outputs:
             output.send(self.logger)
+
+    def draw(self):
+        """Send method substitute from old livelossplot api"""
+        warnings.warn('draw will be deprecated, please use send method', PendingDeprecationWarning)
+        self.send()
