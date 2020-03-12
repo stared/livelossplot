a78fdd95fa19549a2151144d7f9b954b3e46caad
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-03-12 18:04:14 +0100

    keras auto-groups

--- livelossplot/input_plugins/generic_keras.py
+++ livelossplot/input_plugins/generic_keras.py
@@ -7,7 +7,20 @@ class _PlotLossesCallback:
     def __init__(self, **kwargs):
         self.liveplot = PlotLosses(**kwargs)
 
+    @staticmethod
+    def _set_groups(logger, keys):
+        if logger.groups or logger.group_patterns:
+            return
+        groups = {}
+        for key in keys:
+            abs_key = key.replace('val_', '')
+            if not groups.get(abs_key):
+                groups[abs_key] = []
+            groups[abs_key].append(key)
+        logger.groups = groups
+
     def on_epoch_end(self, epoch, logs):
         """Send metrics to livelossplot"""
+        self._set_groups(self.liveplot.logger, logs.keys())
         self.liveplot.update(logs.copy(), epoch)
         self.liveplot.send()
