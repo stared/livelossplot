55aa26f453de1067f8602dff5ac2d9cb367ad402
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-03-17 16:01:49 +0100

    external tests

---
+++ livelossplot/output_plugins/external_test_tensorboard.py
@@ -0,0 +1,26 @@
+import os
+
+import numpy as np
+
+from livelossplot import plot_losses
+from livelossplot.output_plugins import tensorboard
+
+
+def test_tensorboard():
+    groups = {'acccuracy': ['acc', 'val_acc'], 'log-loss': ['loss', 'val_loss']}
+    logger = tensorboard.TensorboardLogger()
+
+    liveplot = plot_losses.PlotLosses(groups=groups, outputs=(logger, ))
+
+    for i in range(3):
+        liveplot.update(
+            {
+                'acc': 1 - np.random.rand() / (i + 2.),
+                'val_acc': 1 - np.random.rand() / (i + 0.5),
+                'loss': 1. / (i + 2.),
+                'val_loss': 1. / (i + 0.5)
+            }
+        )
+        liveplot.send()
+
+    assert all([f.startswith('events.out.tfevents.') for f in os.listdir(logger._path)])
