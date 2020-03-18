5a3fb1650f12a585e1f37072e16df85f91929c9c
Piotr Migdal <pmigdal@gmail.com>
2020-03-18 15:14:01 +0100

    version and default imports

--- livelossplot/__init__.py
+++ livelossplot/__init__.py
@@ -1,5 +1,20 @@
-from .plot_losses import PlotLosses
+import warnings
+from .version import __version__
 
-__all__ = [
-    'PlotLosses',
-]
+from .plot_losses import PlotLosses
+from .main_logger import MainLogger
+
+# TODO 
+# expose keras as before, something like
+# import livelossplot.input_plugins.keras as liveloss.keras
+# or consider undepreciating lines below?
+
+def PlotLossesKeras(*args, **kwargs):
+    warnings.warn("From v0.3 onwards, use:\nfrom livelossplot.keras import PlotLossesCallback", DeprecationWarning)
+    from .input_plugins.keras import PlotLossesCallback
+    return PlotLossesCallback(*args, **kwargs)
+
+def PlotLossesTensorFlowKeras(*args, **kwargs):
+    warnings.warn("New and deprecated at the same time!\nFrom v0.3 onwards, use:\nfrom livelossplot.tf_keras import PlotLossesCallback", DeprecationWarning)
+    from .input_plugins.tf_keras import PlotLossesCallback
+    return PlotLossesCallback(*args, **kwargs)
