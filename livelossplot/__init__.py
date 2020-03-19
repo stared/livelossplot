cbd1399b82e9e27cfa54460cb935cb528b276657
Piotr Migdal <pmigdal@gmail.com>
2020-03-19 18:21:02 +0100

    feat(api) input PlotLossesKeras etc naming

--- livelossplot/__init__.py
+++ livelossplot/__init__.py
@@ -5,27 +5,17 @@ from importlib.util import find_spec
 from .main_logger import MainLogger
 from .plot_losses import PlotLosses
 from . import inputs
+from .inputs import *
 from . import outputs
 from .version import __version__
 
 
-SHORTEN_INPUT_PATHS = [
-    'keras',
-    'tf_keras',
-    'pytorch_ignite',
-    'poutyne',
-]
-
-
-def PlotLossesKeras(**kwargs):
-    from .inputs.keras import PlotLossesCallback
-    return PlotLossesCallback(**kwargs)
-
-
-def PlotLossesTensorFlowKeras(**kwargs):
-    from .inputs.tf_keras import PlotLossesCallback
-    return PlotLossesCallback(**kwargs)
-
+_input_plugin_dict = {
+    'keras': 'Keras',
+    'tf_keras': 'KerasTF',
+    'pytorch_ignite': 'Ignite',
+    'poutyne': 'Poutyne',
+}
 
 class OldDependenciesFinder:
     """
@@ -39,17 +29,16 @@ class OldDependenciesFinder:
         This functions is what gets executed by the loader.
         """
         parts = fullname.split('.')
-        if len(parts) == 2 and parts[0] == 'livelossplot' and parts[1] in SHORTEN_INPUT_PATHS:
-            warnings.warn(
-                'livelossplot.{} will be deprecated, please use livelossplot.inputs.{}'.format(parts[1],
-                                                                                                      parts[1]),
-                DeprecationWarning
-            )
-            fullname = '.'.join(['livelossplot', 'inputs', parts[1]])
+        if len(parts) == 2 and parts[0] == 'livelossplot' and parts[1] in _input_plugin_dict:
+            name = parts[1]
+            msg = 'livelossplot.{name} will be deprecated, please use livelossplot.inputs.{name}\n'
+            msg += 'or use callback directly: from livelossplot import PlotLosses{new_name}'
+            warnings.warn(msg.format(name=name, new_name=_input_plugin_dict[name]), DeprecationWarning)
+            fullname = 'livelossplot.inputs.{name}'.format(name=name)
             return find_spec(fullname)
         return None
 
 
 sys.meta_path.append(OldDependenciesFinder())
 
-__all__ = ['PlotLosses']
+__all__ = ['MainLogger', 'inputs', 'outputs', 'PlotLosses', 'PlotLossesKeras', 'PlotLossesKerasTF', 'PlotLossesIgnite', 'PlotLossesPoutyne']
