dc5f547f770b375a695010c394471b89f6bebaa9
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-03-20 17:35:45 +0100

    remove old group patterns

--- tests/test_main_logger.py
+++ tests/test_main_logger.py
@@ -31,27 +31,25 @@ def test_main_logger_with_groups():
     assert len(grouped_log_history['log-loss']['training ']) == 3
 
 
-def test_main_logger_with_group_patterns():
+def test_main_logger_with_default_groups():
     """Test group patterns"""
-    group_patterns = {'acccuracy': re.compile(r'.*acc$'), 'log-loss': re.compile(r'.*loss$')}
-    logger = MainLogger(group_patterns=group_patterns)
+    logger = MainLogger()
     logger.update({'acc': 0.5, 'val_acc': 0.4, 'loss': 1.2, 'val_loss': 1.1})
     logger.update({'acc': 0.55, 'val_acc': 0.45, 'loss': 1.1, 'val_loss': 1.0})
     logger.update({'acc': 0.65, 'val_acc': 0.55, 'loss': 1.0, 'val_loss': 0.9})
     grouped_log_history = logger.grouped_log_history(raw_names=True, raw_group_names=True)
     assert len(grouped_log_history) == 2
-    assert len(grouped_log_history['acccuracy']) == 2
-    assert len(grouped_log_history['acccuracy']['val_acc']) == 3
+    assert len(grouped_log_history['acc']) == 2
+    assert len(grouped_log_history['acc']['val_acc']) == 3
     grouped_log_history = logger.grouped_log_history()
     assert len(grouped_log_history) == 2
-    assert len(grouped_log_history['acccuracy']) == 2
-    assert len(grouped_log_history['acccuracy']['validation ']) == 3
+    assert len(grouped_log_history['Accuracy']) == 2
+    assert len(grouped_log_history['Accuracy']['validation ']) == 3
 
 
 def test_main_logger_metrc_to_name():
     """Test group patterns"""
-    group_patterns = {'acccuracy': re.compile(r'.*acc$'), 'log-loss': re.compile(r'.*loss$')}
-    logger = MainLogger(group_patterns=group_patterns)
+    logger = MainLogger()
     logger.update({'acc': 0.5, 'val_acc': 0.4, 'loss': 1.2, 'val_loss': 1.1, 'lr': 0.01})
     logger.update({'acc': 0.55, 'val_acc': 0.45, 'loss': 1.1, 'val_loss': 1.0, 'lr': 0.001})
     logger.update({'acc': 0.65, 'val_acc': 0.55, 'loss': 1.0, 'val_loss': 0.9, 'lr': 0.0001})
--- tests/test_plot_losses.py
+++ tests/test_plot_losses.py
@@ -8,19 +8,18 @@ class CheckOutput:
         assert isinstance(logger, MainLogger)
         grouped_log_history = logger.grouped_log_history(raw_names=True, raw_group_names=True)
         assert len(grouped_log_history) == 2
-        assert len(grouped_log_history['accuracy']) == 2
-        assert len(grouped_log_history['accuracy']['val_acc']) == 2
+        assert len(grouped_log_history['acc']) == 2
+        assert len(grouped_log_history['acc']['val_acc']) == 2
         grouped_log_history = logger.grouped_log_history()
         assert len(grouped_log_history) == 2
-        assert len(grouped_log_history['accuracy']) == 2
+        assert len(grouped_log_history['Accuracy']) == 2
         print(grouped_log_history)
-        assert len(grouped_log_history['accuracy']['validation ']) == 2
+        assert len(grouped_log_history['Accuracy']['validation ']) == 2
 
 
 def test_plot_losses():
     """Test basic usage"""
-    group_patterns = {'accuracy': re.compile(r'.*acc$'), 'log-loss': re.compile(r'.*loss$')}
-    loss_plotter = PlotLosses(outputs=(CheckOutput(), ), group_patterns=group_patterns)
+    loss_plotter = PlotLosses(outputs=(CheckOutput(), ))
     loss_plotter.update({'acc': 0.5, 'val_acc': 0.4, 'loss': 1.2, 'val_loss': 1.1})
     loss_plotter.update({
         'acc': 0.55,
