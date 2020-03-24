d7bf647c16c60844a156054ae4e98862ebf8cb19
Piotr Migdał <pmigdal@gmail.com>
2020-03-24 18:42:46 +0100

    test(neptune) test logger using neptune API (#96)
    * test(neptune) logger using neptune API
    
    * chore(yapf)
    
    * test(neptune) added to workflow
    
    * fix(neptune) test code fix
    
    * fix(neptune) close not stop
    
    * fix(neptune) test succeeded

---
+++ tests/external_api_test_neptune.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from livelossplot import PlotLosses
+from livelossplot.outputs import NeptuneLogger
+
+import neptune
+
+
+def test_neptune():
+    neptune_logger = NeptuneLogger(
+        api_token="ANONYMOUS", project_qualified_name="shared/colab-test-run", tags=['livelossplot', 'github-actions']
+    )
+
+    plotlosses = PlotLosses(outputs=[neptune_logger])
+
+    assert neptune_logger.experiment.state == 'running'
+
+    for i in range(3):
+        plotlosses.update(
+            {
+                'acc': 1 - np.random.rand() / (i + 2.),
+                'val_acc': 1 - np.random.rand() / (i + 0.5),
+                'loss': 1. / (i + 2.),
+                'val_loss': 1. / (i + 0.5)
+            }
+        )
+        plotlosses.send()
+
+    assert neptune_logger.experiment.state == 'running'
+
+    neptune_logger.close()
+
+    assert neptune_logger.experiment.state == 'succeeded'
+
+    url = neptune.project._get_experiment_link(neptune_logger.experiment)
+
+    assert len(url) > 0
