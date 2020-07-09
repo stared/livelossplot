d2292eb5ebc27df6fee5304379f64ba0805541fc
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-07-09 10:45:43 +0200

    add decorations to matplotlib output

--- livelossplot/outputs/matplotlib_plot.py
+++ livelossplot/outputs/matplotlib_plot.py
@@ -1,4 +1,5 @@
-from typing import Tuple, List, Dict, Optional
+
+from typing import Tuple, List, Dict, Optional, Callable
 
 import warnings
 
@@ -18,7 +19,10 @@ class MatplotlibPlot(BaseOutput):
         max_epoch: int = None,
         skip_first: int = 2,
         extra_plots=[],
-        figpath: Optional[str] = None
+        figpath: Optional[str] = None,
+        after_subplot: Optional[Callable[[str], ...]] = None,
+        before_show_plot: Optional[Callable[[str], ...]] = None,
+        after_show_plot: Optional[Callable[[str], ...]] = None,
     ):
         self.cell_size = cell_size
         self.max_cols = max_cols
@@ -28,31 +32,48 @@ class MatplotlibPlot(BaseOutput):
         self.max_epoch = max_epoch
         self.figpath = figpath
         self.file_idx = 0  # now only for saving files
+        self._after_subplot = after_subplot if after_subplot else self._default_after_subplot
+        self._before_show_plot = before_show_plot if before_show_plot else self._default_before_show_plot
+        self._after_show_plot = after_show_plot if after_show_plot else self._default_after_show_plot
 
     def send(self, logger: MainLogger):
         """Draw figures with metrics and show"""
         log_groups = logger.grouped_log_history()
-        figsize_x = self.max_cols * self.cell_size[0]
-        figsize_y = ((len(log_groups) + 1) // self.max_cols + 1) * self.cell_size[1]
+        x_labels = logger.step_names
 
         max_rows = (len(log_groups) + len(self.extra_plots) + 1) // self.max_cols + 1
 
         clear_output(wait=True)
-        plt.figure(figsize=(figsize_x, figsize_y))
+        self._before_show_plot(len(log_groups))
 
-        for group_id, (group_name, group_logs) in enumerate(log_groups.items()):
-            plt.subplot(max_rows, self.max_cols, group_id + 1)
+        for group_idx, (group_name, group_logs) in enumerate(log_groups.items()):
+            plt.subplot(max_rows, self.max_cols, group_idx + 1)
             self._draw_metric_subplot(group_logs, group_name=group_name)
+            self._after_subplot(group_name)
 
-        for i, extra_plot in enumerate(self.extra_plots):
-            plt.subplot(max_rows, self.max_cols, i + len(log_groups) + 1)
+        for idx, extra_plot in enumerate(self.extra_plots):
+            plt.subplot(max_rows, self.max_cols, idx + len(log_groups) + 1)
             extra_plot(logger)
 
-        plt.tight_layout()
+        self._after_show_plot()
         if self.figpath is not None:
             plt.savefig(self.figpath.format(i=self.file_idx))
             self.file_idx += 1
+
         plt.show()
+
+    def _default_after_subplot(self, group_name: str):
+        plt.title(group_name)
+        plt.xlabel('epoch')
+        plt.legend(loc='center right')
+
+    def _default_before_show_plot(self, num_of_log_groups: int) -> None:
+        figsize_x = self.max_cols * self.cell_size[0]
+        figsize_y = ((num_of_log_groups + 1) // self.max_cols + 1) * self.cell_size[1]
+        plt.figure(figsize=(figsize_x, figsize_y))
+
+    def _default_after_show_plot(self):
+        plt.tight_layout()
 
     def _draw_metric_subplot(self, group_logs: Dict[str, List[LogItem]], group_name: str = ''):
         # there used to be skip first part, but I skip it first
@@ -65,9 +86,7 @@ class MatplotlibPlot(BaseOutput):
                 ys = [log.value for log in logs]
                 plt.plot(xs, ys, label=name)
 
-        plt.title(group_name)
-        plt.xlabel('epoch')
-        plt.legend(loc='center right')
+        self._after_subplot(group_name)
 
     def _not_inline_warning(self):
         backend = matplotlib.get_backend()
