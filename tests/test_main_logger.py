6202511ff4c3918991ad194f8031d4b6c29c4772
Bartłomiej Olechno <bartlomiej.olechno@gmail.com>
2020-07-10 14:46:06 +0200

    fix tests for new default group patterns

--- tests/test_main_logger.py
+++ tests/test_main_logger.py
@@ -28,7 +28,7 @@ def test_main_logger_with_groups():
     assert len(grouped_log_history) == 2
     assert len(grouped_log_history['acccuracy']) == 2
     assert len(grouped_log_history['acccuracy']['validation ']) == 3
-    assert len(grouped_log_history['log-loss']['training ']) == 3
+    assert len(grouped_log_history['log-loss']['training']) == 3
 
 
 def test_main_logger_with_default_groups():
@@ -44,7 +44,7 @@ def test_main_logger_with_default_groups():
     grouped_log_history = logger.grouped_log_history()
     assert len(grouped_log_history) == 2
     assert len(grouped_log_history['Accuracy']) == 2
-    assert len(grouped_log_history['Accuracy']['validation ']) == 3
+    assert len(grouped_log_history['Accuracy']['validation']) == 3
 
 
 def test_main_logger_metrc_to_name():
@@ -56,10 +56,10 @@ def test_main_logger_metrc_to_name():
     metric_to_name = logger.metric_to_name
     assert 'lr' not in metric_to_name
     target_metric_to_name = {
-        'acc': 'training ',
-        'val_acc': 'validation ',
-        'loss': 'training ',
-        'val_loss': 'validation ',
+        'acc': 'training',
+        'val_acc': 'validation',
+        'loss': 'training',
+        'val_loss': 'validation',
     }
     for metric, metric_name in metric_to_name.items():
         assert metric_name == target_metric_to_name.get(metric)
@@ -72,7 +72,7 @@ def test_main_logger_autogroups():
     logger.update({'acc': 0.55, 'val_acc': 0.45, 'loss': 1.1, 'val_loss': 1.0, 'lr': 0.001})
     logger.update({'acc': 0.65, 'val_acc': 0.55, 'loss': 1.0, 'val_loss': 0.9, 'lr': 0.0001})
     grouped_log_history = logger.grouped_log_history()
-    target_groups = {'Accuracy': ('validation ', 'training '), 'Loss': ('validation ', 'training '), 'lr': ('lr', )}
+    target_groups = {'Accuracy': ('validation', 'training'), 'Loss': ('validation', 'training'), 'lr': ('lr', )}
     for target_group, target_metrics in target_groups.items():
         for m1, m2 in zip(sorted(grouped_log_history[target_group].keys()), sorted(target_metrics)):
             assert m1 == m2
