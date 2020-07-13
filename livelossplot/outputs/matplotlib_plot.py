77fd9f4fbde745fa239fc73bd0b9f203d5dabf2b
bartolo1024 <bartlomiej.olechno@gmail.com>
2020-07-13 13:22:09 +0200

    matplotlib with axes api && example

--- livelossplot/outputs/matplotlib_plot.py
+++ livelossplot/outputs/matplotlib_plot.py
@@ -19,9 +19,9 @@ class MatplotlibPlot(BaseOutput):
         skip_first: int = 2,
         extra_plots: List[Callable[[MainLogger], None]] = [],
         figpath: Optional[str] = None,
-        after_subplot: Optional[Callable[[str, str], None]] = None,
-        before_plots: Optional[Callable[[int], None]] = None,
-        after_plots: Optional[Callable[[], None]] = None,
+        after_subplot: Optional[Callable[[plt.Axes, str, str], None]] = None,
+        before_plots: Optional[Callable[[plt.Figure, int], None]] = None,
+        after_plots: Optional[Callable[[plt.Figure], None]] = None,
     ):
         """
         :param cell_size size of one chart:
@@ -44,60 +44,62 @@ class MatplotlibPlot(BaseOutput):
         self._after_subplot = after_subplot if after_subplot else self._default_after_subplot
         self._before_plots = before_plots if before_plots else self._default_before_plots
         self._after_plots = after_plots if after_plots else self._default_after_plots
-
+        self._axes_shape = None
 
     def send(self, logger: MainLogger):
         """Draw figures with metrics and show"""
         log_groups = logger.grouped_log_history()
 
-        max_rows = (len(log_groups) + len(self.extra_plots) + 1) // self.max_cols + 1
+        max_rows = (len(log_groups) + len(self.extra_plots)) // self.max_cols
 
-        self._before_plots(len(log_groups))
+        fig, axes = plt.subplots(max_rows, self.max_cols)
+        axes = axes.reshape(-1, self.max_cols)
+        self._before_plots(fig, len(log_groups))
 
         for group_idx, (group_name, group_logs) in enumerate(log_groups.items()):
-            plt.subplot(max_rows, self.max_cols, group_idx + 1)
-            self._draw_metric_subplot(group_logs, group_name=group_name, x_label=logger.step_names[group_name])
+            ax = axes[group_idx // self.max_cols, group_idx % self.max_cols]
+            self._draw_metric_subplot(ax, group_logs, group_name=group_name, x_label=logger.step_names[group_name])
 
         for idx, extra_plot in enumerate(self.extra_plots):
-            plt.subplot(max_rows, self.max_cols, idx + len(log_groups) + 1)
-            extra_plot(logger)
+            ax = axes[(len(log_groups) + idx) // self.max_cols, (len(log_groups) + idx) % self.max_cols]
+            extra_plot(ax, logger)
 
-        self._after_plots()
+        self._after_plots(fig)
         if self.figpath is not None:
-            plt.savefig(self.figpath.format(i=self.file_idx))
+            fig.savefig(self.figpath.format(i=self.file_idx))
             self.file_idx += 1
 
         plt.show()
 
-    def _default_after_subplot(self, group_name: str, x_label: str):
+    def _default_after_subplot(self, ax: plt.Axes, group_name: str, x_label: str):
         """Add title xlabel and legend to single chart"""
-        plt.title(group_name)
-        plt.xlabel(x_label)
-        plt.legend(loc='center right')
+        ax.set_title(group_name)
+        ax.set_xlabel(x_label)
+        ax.legend(loc='center right')
 
-    def _default_before_plots(self, num_of_log_groups: int) -> None:
+    def _default_before_plots(self, figure: plt.Figure, num_of_log_groups: int) -> None:
         """Set matplotlib window properties"""
         clear_output(wait=True)
         figsize_x = self.max_cols * self.cell_size[0]
         figsize_y = ((num_of_log_groups + 1) // self.max_cols + 1) * self.cell_size[1]
-        plt.figure(figsize=(figsize_x, figsize_y))
+        figure.set_size_inches(figsize_x, figsize_y)
 
-    def _default_after_plots(self):
+    def _default_after_plots(self, figure: plt.Figure):
         """Set properties after charts creation"""
-        plt.tight_layout()
+        figure.tight_layout()
 
-    def _draw_metric_subplot(self, group_logs: Dict[str, List[LogItem]], group_name: str, x_label: str):
+    def _draw_metric_subplot(self, ax: plt.Axes, group_logs: Dict[str, List[LogItem]], group_name: str, x_label: str):
         # there used to be skip first part, but I skip it first
         if self.max_epoch is not None:
-            plt.xlim(0, self.max_epoch)
+            ax.set_xlim(0, self.max_epoch)
 
         for name, logs in group_logs.items():
             if len(logs) > 0:
                 xs = [log.step for log in logs]
                 ys = [log.value for log in logs]
-                plt.plot(xs, ys, label=name)
+                ax.plot(xs, ys, label=name)
 
-        self._after_subplot(group_name, x_label)
+        self._after_subplot(ax, group_name, x_label)
 
     def _not_inline_warning(self):
         backend = matplotlib.get_backend()
