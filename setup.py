99699427693299a507a152f094720410984dea1b
Bartlomiej Olechno <bartlomiej.olechno@gmail.com>
2021-02-02 23:58:08 +0100

    try to solve numpy dependecy for python 3.6

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         'ipython', 'matplotlib', 'bokeh',
+        'numpy<=1.19;python_version="3.6"',
     ],
     description='Live training loss plot in Jupyter Notebook for Keras, PyTorch and others.',
     long_description=readme(),
