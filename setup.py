9a32561211959f041873dbf73b5217ea91abda3d
Frédérik Paradis <fredy_14@live.fr>
2022-04-04 11:47:20 -0400

    Add Python 3.7 in setuptools classifiers

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Topic :: Scientific/Engineering :: Visualization',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
