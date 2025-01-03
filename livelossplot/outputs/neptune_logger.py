110bc8b09086092523b44f8068031dcac7f3034e
Piotr Migdal <pmigdal@gmail.com>
2025-01-03 15:01:47 +0100

    neptune integration update

--- livelossplot/outputs/neptune_logger.py
+++ livelossplot/outputs/neptune_logger.py
@@ -7,7 +7,13 @@ class NeptuneLogger(BaseOutput):
     """See: https://github.com/neptune-ai/neptune-client
     YOUR_API_TOKEN and USERNAME/PROJECT_NAME
     """
-    def __init__(self, api_token: Optional[str] = None, project_qualified_name: Optional[str] = None, **kwargs):
+
+    def __init__(
+        self,
+        api_token: Optional[str] = None,
+        project_qualified_name: Optional[str] = None,
+        **kwargs
+    ):
         """Set secrets and create experiment
         Args:
             api_token: your api token, you can create NEPTUNE_API_TOKEN environment variable instead
@@ -15,16 +21,19 @@ class NeptuneLogger(BaseOutput):
             **kwargs: keyword args, that will be passed to create_experiment function
         """
         import neptune
+
         self.neptune = neptune
-        self.neptune.init(api_token=api_token, project_qualified_name=project_qualified_name)
-        self.experiment = self.neptune.create_experiment(**kwargs)
+        self.run = self.neptune.init_run(
+            api_token=api_token, project=project_qualified_name, **kwargs
+        )
 
     def close(self):
         """Close connection"""
-        self.neptune.stop()
+        if hasattr(self, "run"):
+            self.run.stop()
 
     def send(self, logger: MainLogger):
         """Send metrics collected in last step to neptune server"""
         for name, log_items in logger.log_history.items():
             last_log_item = log_items[-1]
-            self.neptune.send_metric(name, x=last_log_item.step, y=last_log_item.value)
+            self.run[name].append(value=last_log_item.value, step=last_log_item.step)
