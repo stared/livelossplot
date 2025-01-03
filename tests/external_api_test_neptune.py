1e7f1eae79146c051ad405203b788f432c04d9d3
Piotr Migdal <pmigdal@gmail.com>
2025-01-03 15:10:03 +0100

    neptune test fix

--- tests/external_api_test_neptune.py
+++ tests/external_api_test_neptune.py
@@ -8,31 +8,31 @@ import neptune
 
 def test_neptune():
     neptune_logger = NeptuneLogger(
-        api_token="ANONYMOUS", project_qualified_name="shared/colab-test-run", tags=['livelossplot', 'github-actions']
+        api_token="ANONYMOUS",
+        project_qualified_name="shared/colab-test-run",
+        tags=["livelossplot", "github-actions"],
     )
 
     plotlosses = PlotLosses(outputs=[neptune_logger])
 
-    assert neptune_logger.experiment.state == 'running'
+    assert neptune_logger.run.exists
 
     for i in range(3):
         plotlosses.update(
             {
-                'acc': 1 - np.random.rand() / (i + 2.),
-                'val_acc': 1 - np.random.rand() / (i + 0.5),
-                'loss': 1. / (i + 2.),
-                'val_loss': 1. / (i + 0.5)
+                "acc": 1 - np.random.rand() / (i + 2.0),
+                "val_acc": 1 - np.random.rand() / (i + 0.5),
+                "loss": 1.0 / (i + 2.0),
+                "val_loss": 1.0 / (i + 0.5),
             }
         )
         plotlosses.send()
 
-    assert neptune_logger.experiment.state == 'running'
+    assert neptune_logger.run.exists
 
     neptune_logger.close()
 
-    # This is not working anymore.
-    # assert neptune_logger.experiment.state == 'succeeded'
-
-    url = neptune.project._get_experiment_link(neptune_logger.experiment)
+    # Get the run URL
+    url = neptune_logger.run.get_url()
 
     assert len(url) > 0
